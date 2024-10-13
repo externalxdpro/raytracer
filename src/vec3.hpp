27e2ee1f087ab19acb6a36e88641ff9493ffcce4
externalxdpro <externalxdpro@gmail.com>
2024-10-13 17:25:31 -0400

    Make a bunch of functions const

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -12,7 +12,7 @@ class Vec3 {
     Vec3() : x(0), y(0), z(0) {}
     Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
-    const Vec3 operator-() { return Vec3(-x, -y, -z); }
+    Vec3 operator-() const { return Vec3(-x, -y, -z); }
 
     Vec3 &operator+=(const Vec3 &other) {
         this->x += other.x;
@@ -30,39 +30,43 @@ class Vec3 {
 
     Vec3 &operator/=(double t) { return *this *= 1 / t; }
 
-    const double length() { return std::sqrt(length_squared()); }
-    const double length_squared() { return x * x + y * y + z * z; }
+    const double length() const { return std::sqrt(this->length_squared()); }
+    const double length_squared() const {
+        return this->x * this->x + this->y * this->y + this->z * this->z;
+    }
 
-    std::ostream &operator<<(std::ostream &out) {
+    std::ostream &operator<<(std::ostream &out) const {
         return out << std::format("{} {} {}", this->x, this->y, this->z);
     }
 
-    Vec3 operator+(const Vec3 &other) {
+    Vec3 operator+(const Vec3 &other) const {
         return {this->x + other.x, this->y + other.y, this->z + other.z};
     }
 
-    Vec3 operator-(const Vec3 &other) {
+    Vec3 operator-(const Vec3 &other) const {
         return {this->x - other.x, this->y - other.y, this->z - other.z};
     }
 
-    Vec3 operator*(const Vec3 &other) {
+    Vec3 operator*(const Vec3 &other) const {
         return {this->x * other.x, this->y * other.y, this->z * other.z};
     }
-    Vec3 operator*(double t) { return {t * this->x, t * this->y, t * this->z}; }
+    Vec3 operator*(double t) const {
+        return {t * this->x, t * this->y, t * this->z};
+    }
 
-    Vec3 operator/(double t) { return *this * (1 / t); }
+    Vec3 operator/(double t) const { return *this * (1 / t); }
 
-    double dot(const Vec3 &other) {
+    double dot(const Vec3 &other) const {
         return this->x * other.x + this->y * other.y + this->z * other.z;
     }
 
-    Vec3 cross(const Vec3 &other) {
+    Vec3 cross(const Vec3 &other) const {
         return {this->y * other.z - this->z * other.y,
                 this->z * other.x - this->x * other.z,
                 this->x * other.y - this->y * other.x};
     }
 
-    Vec3 unitVector() { return *this / this->length(); }
+    Vec3 unitVector() const { return *this / this->length(); }
 };
 
 #endif // VEC3_H_
