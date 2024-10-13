96b5153807a6295ead38052646537d46377d9fa2
externalxdpro <externalxdpro@gmail.com>
2024-10-13 17:13:05 -0400

    Set up viewport

--- src/colour.hpp
+++ src/colour.hpp
@@ -1,12 +1,19 @@
 #ifndef COLOUR_H_
 #define COLOUR_H_
 
+#include "ray.hpp"
 #include "vec3.hpp"
 
 #include <format>
 #include <iostream>
 
 using Colour = Vec3;
+
+inline Colour rayColour(Ray &r) {
+    Vec3   unitDir = r.dir.unitVector();
+    double a       = (unitDir.y + 1) / 2;
+    return Colour(1, 1, 1) * (1.0 - a) + Colour(0.5, 0.7, 1.0) * a;
+}
 
 inline void writeColour(std::ostream &out, const Colour &pixelColour) {
     int r = 255.999 * pixelColour.x;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include "colour.hpp"
+#include "ray.hpp"
 #include "vec3.hpp"
 
 #include <format>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    const int imgWidth = 256, imgHeight = 256;
+    const double aspectRatio = 16.0 / 9.0;
+    const int    imgWidth    = 400;
+
+    int imgHeight = int(imgWidth / aspectRatio);
+    imgHeight     = (imgHeight < 1) ? 1 : imgHeight;
+
+    double focalLength    = 1;
+    double viewportHeight = 2;
+    double viewportWidth  = viewportHeight * (double)(imgWidth) / imgHeight;
+    Vec3   camCenter      = {0, 0, 0};
+
+    Vec3 viewportU = {viewportWidth, 0, 0};
+    Vec3 viewportV = {0, -viewportHeight, 0};
+
+    Vec3 pixelDeltaU = viewportU / imgWidth;
+    Vec3 pixelDeltaV = viewportV / imgHeight;
+
+    Vec3 viewportTopLeft =
+        camCenter - Vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
+    Vec3 pixel0Pos = viewportTopLeft + (pixelDeltaU + pixelDeltaV) / 2;
 
     std::cout << "P3\n";
     std::cout << std::format("{} {}\n", imgWidth, imgHeight);
@@ -15,8 +35,12 @@ int main(int argc, char *argv[]) {
         std::clog << std::format("\rScanlines remaining: {} ", imgHeight - j)
                   << std::flush;
         for (int i = 0; i < imgWidth; i++) {
-            Colour pixelColour = {(double)i / (imgWidth - 1),
-                                  (double)j / (imgHeight - 1), 0};
+            Vec3 pixelCenter =
+                pixel0Pos + (pixelDeltaU * i) + (pixelDeltaV * j);
+            Vec3 rayDir = pixelCenter - camCenter;
+            Ray  r      = {camCenter, rayDir};
+
+            Colour pixelColour = rayColour(r);
             writeColour(std::cout, pixelColour);
         }
     }
