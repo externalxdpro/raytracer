cdf6e99bc6ed0f5b058ece47e1655f1b04ae8e5e
externalxdpro <externalxdpro@gmail.com>
2024-10-13 19:56:19 -0400

    Add random number generator functions

--- src/util.hpp
+++ src/util.hpp
@@ -6,11 +6,22 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 const double inf = std::numeric_limits<double>::infinity();
 const double pi  = M_PI;
 
 inline double degToRad(double deg) { return deg * pi / 180.0; }
+
+inline double randDouble() {
+    static std::uniform_real_distribution<double> dist(0, 1);
+    static std::mt19937                           gen;
+    return dist(gen);
+}
+
+inline double randDouble(double min, double max) {
+    return min + (max - min) * randDouble();
+}
 
 #include "colour.hpp"
 #include "interval.hpp"
