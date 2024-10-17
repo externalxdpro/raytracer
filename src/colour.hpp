c4878b5c36b360c5275e7ec0d6e1f686de7b7238
externalxdpro <externalxdpro@gmail.com>
2024-10-17 13:51:32 -0400

    Add gamma correction

--- src/colour.hpp
+++ src/colour.hpp
@@ -4,16 +4,25 @@
 #include "interval.hpp"
 #include "vec3.hpp"
 
+#include <cmath>
+
 using Colour = Vec3;
+
+inline double linearToGamma(double linearComponent) {
+    if (linearComponent > 0) {
+        return std::sqrt(linearComponent);
+    }
+    return 0;
+}
 
 inline void writeColour(std::ostream &out, const Colour &pixelColour) {
     static const Interval intensity(0, 0.999);
 
-    int rbyte = 256 * intensity.clamp(pixelColour.x);
-    int gbyte = 256 * intensity.clamp(pixelColour.y);
-    int bbyte = 256 * intensity.clamp(pixelColour.z);
+    int r = 256 * intensity.clamp(linearToGamma(pixelColour.x));
+    int g = 256 * intensity.clamp(linearToGamma(pixelColour.y));
+    int b = 256 * intensity.clamp(linearToGamma(pixelColour.z));
 
-    out << std::format("{} {} {}\n", rbyte, gbyte, bbyte);
+    out << std::format("{} {} {}\n", r, g, b);
 }
 
 #endif // COLOUR_H_
